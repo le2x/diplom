a0f1a775fbe0d473fd2dd68dd6aedb9b3b200fee
sergey lazurenko <lazur2008@yandex.ru>
2017-06-06 17:57:31 +0300

    begin of sculpturing final variant

--- Diplom_robot_v1/Diplom_robot_v1.cpp
+++ Diplom_robot_v1/Diplom_robot_v1.cpp
@@ -6,11 +6,12 @@
  */ 
 
 #ifndef F_CPU
-#define F_CPU 1000000UL	//	рабочая частота
+#define F_CPU 1000000UL	//	рабочая частота must be 8000000
 #define GROUND_LEFT_MOTOR PA7
 #define POWER_LEFT_MOTOR PA6
 #define GROUND_RIGHT_MOTOR PA5
-#define POWER_LEFT_MOTOR PA4
+#define POWER_RIGHT_MOTOR PA4
+#define COMPARE_RATE 20 // rate that regulated sensitivity of PWM balancing
 #endif
 
 #include <stdio.h>
@@ -19,11 +20,10 @@
 #include <stdlib.h>
 #include <avr/interrupt.h>
 
-int int1Led = 1;
-int int0Led = 1;
-
-int WholePWM = 0, LeftBalancePWM = 0, RightBalancePWM = 0, Spiral = 1;
-int impulseLeftPerWheel = 0, impulseLeftPerWheeltemp = 0, impulseRightPerWheel = 0, impulseRightPerWheeltemp = 0; //переменные для скорости вращения колёс
+int WholePWM = 192, LeftBalancePWM = 0, RightBalancePWM = 0, Spiral = 1; // +-63 to balance PWM; wholePWM = 75% of power
+int impulseLeftPerWheel = 0, impulseLeftPerWheeltemp = 0, impulseRightPerWheel = 0, impulseRightPerWheeltemp = 0, compareTemp = 0; //переменные для скорости вращения колёс
+bool isFirstInt1 = false, isFirstInt0 = false; //?
+bool isImpulseLeftUsed = false, isImpulseRightUsed = false; // each int must be compared only once
 
 
 void init_UART()
@@ -31,86 +31,10 @@ void init_UART()
 	//	Установка скорости 9600
 	UBRRH=0;	//	UBRR=f/(16*band)-1 f=8000000Гц band=9600, 
 	UBRRL=51;	//	нормальный асинхронный двунаправленный режим работы
-	
-//			RXC			-	завершение приёма
-//			|TXC		-	завершение передачи
-//			||UDRE 		-	отсутствие данных для отправки
-//			|||FE		-	ошибка кадра
-//			||||DOR		-	ошибка переполнение буфера
-//			|||||PE		-	ошибка чётности
-//			||||||U2X	-	Двойная скорость
-//			|||||||MPCM	-	Многопроцессорный режим
-//			76543210
 	UCSRA=0b00000000;
-
-//			RXCIE		-	прерывание при приёме данных
-//			|TXCIE		-	прерывание при завершение передачи
-//			||UDRIE		-	прерывание отсутствие данных для отправки
-//			|||RXEN		-	разрешение приёма
-//			||||TXEN	-	разрешение передачи
-//			|||||UCSZ2	-	UCSZ0:2 размер кадра данных
-//			||||||RXB8	-	9 бит принятых данных
-//			|||||||TXB8	-	9 бит переданных данных
-//			76543210
 	UCSRB=0b00011000;	//	разрешен приём и передача по UART
-
-//			URSEL		-	всегда 1
-//			|UMSEL		-	режим:1-синхронный 0-асинхронный
-//			||UPM1		-	UPM0:1 чётность
-//			|||UPM0		-	UPM0:1 чётность
-//			||||USBS	-	топ биты: 0-1, 1-2
-//			|||||UCSZ1	-	UCSZ0:2 размер кадра данных
-//			||||||UCSZ0	-	UCSZ0:2 размер кадра данных
-//			|||||||UCPOL-	в синхронном режиме - тактирование
-//			76543210
 	UCSRC=0b10000110;	//	8-битовая посылка
 }
-
-void init_pwm()
-{
-	TIMSK |= (1<<TOIE2)|(1<<TOIE0);
-//	TCCR0|=(1<<WGM00)|(1<<WGM01)|(1<<COM01)|(1<<CS01);
-//	OCR0 = 0x00; //начальное состояние 0
-	
-//	TCCR2|=(1<<WGM20)|(1<<WGM21)|(1<<COM21)|(1<<CS21);
-//	OCR2 = 0x00; //начальное состояние 0
-	
-	TCCR1A = (1<<COM1A1)|(1<<WGM10)|(1<<COM1B1)|(1<<WGM12);
-	TCCR1B = (1<<CS11); //предделитель = 1
-	OCR1A = 0x00; //начальное состо¤ние 0
-	
-	DDRD |= (1<<PD4)|(1<<PD5);
-	//DDRB |= (1<<PB3);
-}
-
-void int0_init( void )
-{
-	//настраиваем на срабатывание INT0 по переднему фронту
-	MCUCR |= (1<<ISC01)|(1<<ISC00);
-	//разрешаем внешнее прерывание INT0
-	GICR |= (1<<INT0);
-	sei();
-}
-
-ISR( INT0_vect )
-{
-	int0Led++;
-		//PORTC = 0b00000001;			//	Включаем диод PC0 = 1 = Vcc
-}
-
-ISR( INT1_vect ){
-	int0Led++;
-	//PORTC = 0b00000001;			//	Включаем диод PC0 = 1 = Vcc
-}
-
-ISR(TIMER0_OVF_vect){
-	
-}
-
-ISR(TIMER2_OVF_vect){
-	
-}
-
 
 //	UART
 void send_Uart(unsigned char c)//	Отправка байта
@@ -144,6 +68,101 @@ unsigned char getch_Uart()//	Получение байта
 	return UDR;
 }
 
+void init_pwm()
+{
+//	TCCR0|=(1<<WGM00)|(1<<WGM01)|(1<<COM01)|(1<<CS01);
+//	OCR0 = 0x00; //начальное состояние 0
+	
+//	TCCR2|=(1<<WGM20)|(1<<WGM21)|(1<<COM21)|(1<<CS21);
+//	OCR2 = 0x00; //начальное состояние 0
+	
+	TCCR1A = (1<<COM1A1)|(1<<WGM10)|(1<<COM1B1)|(1<<WGM12);
+	TCCR1B = (1<<CS11); //предделитель = 1
+	OCR1A = 0x00; //начальное состо¤ние 0
+	
+	DDRD |= (1<<PD4)|(1<<PD5);
+	
+	OCR1A = (WholePWM+RightBalancePWM)*Spiral;
+	OCR1B = WholePWM+RightBalancePWM;
+	//DDRB |= (1<<PB3);
+}
+
+void initTimer(){
+	TIMSK |= (1<<TOIE0);
+	TCCR0|=(1<<CS00);
+}
+
+void externalIntInit( void )
+{
+	//настраиваем на срабатывание INT0 по переднему фронту
+	MCUCR |= (1<<ISC01)|(1<<ISC00)|(1<<ISC11)|(1<<ISC10);
+	//разрешаем внешнее прерывание INT0
+	GICR |= (1<<INT0)|(1<<INT1);
+	sei();
+}
+
+ISR( INT0_vect )
+{
+	impulseLeftPerWheeltemp = impulseLeftPerWheel;
+	isImpulseLeftUsed = false;
+	send_Uart_str("impulseLeft = ");
+	send_int_Uart(impulseLeftPerWheel);
+	send_Uart_str(" OCR1A = ");
+	send_int_Uart(OCR1A);
+	send_Uart(13);
+	impulseLeftPerWheel = 0;
+}
+
+ISR( INT1_vect ){
+	impulseRightPerWheeltemp = impulseRightPerWheel;
+	isImpulseRightUsed = false;
+	send_Uart_str("impulseRight = ");
+	send_int_Uart(impulseRightPerWheel);
+	send_Uart_str(" OCR1B = ");
+	send_int_Uart(OCR1B);
+	send_Uart(13);
+	impulseRightPerWheel = 0;
+}
+
+ISR(TIMER0_OVF_vect){
+	impulseLeftPerWheel++;
+	impulseRightPerWheel++;
+}
+
+void MoveForward(){
+	send_Uart_str("MoveForward");	//	отправка строки
+	send_Uart(13);					//	перенос строки
+	PORTA |= (1<<POWER_LEFT_MOTOR) | (1<<POWER_RIGHT_MOTOR); 
+	if(!isImpulseLeftUsed && !isImpulseRightUsed){
+	compareTemp = impulseLeftPerWheeltemp - impulseRightPerWheeltemp;
+	isImpulseLeftUsed = true; isImpulseRightUsed = true;
+	impulseLeftPerWheeltemp = 0; impulseRightPerWheeltemp = 0;
+	}
+	if (compareTemp<0-COMPARE_RATE){
+		if (OCR1A < 255){
+			LeftBalancePWM += 5;
+		}else{
+			RightBalancePWM -=5;
+	}
+	if (compareTemp>0+COMPARE_RATE){
+		if(OCR1B< 255){
+			RightBalancePWM += 5;
+		}else{
+			LeftBalancePWM-=5;
+		}
+	}
+	compareTemp = 0;	
+	OCR1A = (WholePWM+LeftBalancePWM)*Spiral;
+	OCR1B = WholePWM+RightBalancePWM;
+}
+
+void PortsInit(){
+	DDRA |= (1<<PA4)|(1<<PA5)|(1<<PA6)|(1<<PA7); // set driver outputs to exit
+	DDRC |= (1<<PC7); // signaling led
+	PORTA |= (1<<POWER_LEFT_MOTOR) | (1<<POWER_RIGHT_MOTOR); 
+}
+
+
 int main()
 {
 	init_UART();					//	инициализация UART
@@ -154,17 +173,10 @@ int main()
 	send_Uart(13);					//	перенос строки
 	
 	init_pwm();
-	int0_init();
-	
-	DDRA = 0xFF;
-	DDRC |= (1<<PC7);
-	PORTA |= (1<<PA4) | (1<<PA6);
-	
-	WholePWM = 180;
-	OCR2 = (WholePWM+LeftBalancePWM)*Spiral;
-	OCR0 = WholePWM+RightBalancePWM;
-	OCR1A = WholePWM+RightBalancePWM;
-	OCR1B = WholePWM+RightBalancePWM;
+	externalIntInit();
+	initTimer();
+	PortsInit();
+
 	send_int_Uart(OCR2);
 	send_int_Uart(OCR0);
 	
@@ -172,104 +184,31 @@ int main()
 	_delay_ms(1000);
 	PORTC &= ~(1<<PC7);
 	_delay_ms(1000);
-	/*DDRC = 0xff;				//	инициализация порта C, PC0 - выход
-	DDRB = 0xff;
-	DDRD &= (1<<PD2);
-	PORTD |= (1<<PD2);
-	init_pwm();
-	init_extInt();*/
-	/*init_UART();					//	инициализация UART
-	_delay_ms(1000);				//	задержка 1c
-	send_Uart_str("alex-exe.ru");	//	отправка строки
-	send_Uart(13);					//	перенос строки
-	send_int_Uart(2013);			//	отправка числа
-	send_Uart(13);					//	перенос строки
-   
-   send_Uart_str("AT+CWMODE=3");   //устанавливаем режим wi-fi клиент+точка доступа
-   send_Uart(13);
-   _delay_ms(500);
-   
-   send_Uart_str("AT+CIPMODE=0");  //устанавливаем режим двухстороннего обмена данными для ip сервера
-   send_Uart(13);
-   _delay_ms(500);
-   
-   send_Uart_str("AT+CIPMUX=1");  //устанавливаем режим нескольких соединений для ip сервера
-   send_Uart(13);
-   _delay_ms(500);
-   
-   send_Uart_str("AT+CIPSERVER=1,8888");  //создаем ip сервер на порте 8888
-   send_Uart(13);
-   _delay_ms(500);
      
-   char buff;
-   */
-	
-	/* interrupt testing
-		//временная переменная
-		unsigned long tmp;
-		DDRC = 0b00000001;				//	инициализация порта C, PC0 - выход
-
-
-		int0_init();
-
-		
-
-		while(1) {
-			
-			printf( "int0_cnt = %lu\r\n", tmp );
-			//пауза 1 секунда
-			PORTC = 0b00000000;			//	Выключаем диод PC0 = 0 = Vcc
-		}
-		return 0;
-		PORTC = 0b00000000;		
-		/*if(i<255){
-			OCR1A++;
-			i++;
-			_delay_ms(250);
-		}else{
-			OCR1A = 0x00;
-			i=0;
-		}
-		*/
-		// send_Uart_str("hello");  //создаем ip сервер на порте 8888
-		 //send_Uart(13);
-		/*if(UCSRA&(1<<RXC))			//	если пришёл байт по UART
-		{
-			buff = getch_Uart();
-			send_Uart(buff+1);
-			if (buff == 'a')
-			{
-				PORTC |= (1<<PC1);
-			}
-			if (buff == 'w')
-			{
-				PORTC |= (1<<PC2);
-			}
-				//	отвечаем увеличенным на 1 пришедшим байтом
-			// LED on					//	и мигнём диодом
-			PORTC |= (1<<PC0);			//	Включаем диод PC0 = 1 = Vcc
-			_delay_ms(50);				//	задержка 50мс
-			// LED off
-			PORTC &= ~(1<<PC0);			//	Выключаем диод PC0 = 0 = Vcc
-		}	*/
-		uint8_t i = 0;
-		int iterator = 0;
-		while(1){
-			/*
-			 i = PIND;
-			if((1<<PD2) & i) {
-				int0Led++;
-				PORTC |= (1<<PC7);
-				_delay_ms(1000);
-				PORTC &= ~(1<<PC7);
-				_delay_ms(1000);
-			}
-			*/
-			if((iterator%100) == 0){
-				send_int_Uart(int0Led);			//	отправка числа
-				send_Uart(13);					//	перенос строки
-			}
-			
+  	while(1){
+			MoveForward();
 		}
 			
 }
+
+//    send_Uart_str("AT+CWMODE=3");   //устанавливаем режим wi-fi клиент+точка доступа
+   //    send_Uart(13);
+   //    _delay_ms(500);
+   //    
+   //    send_Uart_str("AT+CIPMODE=0");  //устанавливаем режим двухстороннего обмена данными для ip сервера
+   //    send_Uart(13);
+   //    _delay_ms(500);
+   //    
+   //    send_Uart_str("AT+CIPMUX=1");  //устанавливаем режим нескольких соединений для ip сервера
+   //    send_Uart(13);
+   //    _delay_ms(500);
+   //    
+   //    send_Uart_str("AT+CIPSERVER=1,8888");  //создаем ip сервер на порте 8888
+   //    send_Uart(13);
+   //    _delay_ms(500);
+   
+   /*
+			 i = PIND;
+			if((1<<PD2) & i) {
+			}
+			*/
